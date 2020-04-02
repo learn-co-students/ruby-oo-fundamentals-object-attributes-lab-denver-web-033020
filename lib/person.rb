8537390f62e9b1bc3b8c4c703352a9b68406206e
wolf-dominion <hannah_drury@yahoo.com>
2020-04-02 09:54:50 -0600

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+
+class Person
+    def name=(personsName)
+        @name = personsName
+    end
+    
+    def name
+        @name
+    end
+
+    def job=(theirJob)
+        @job = theirJob
+    end
+
+    def job
+        @job
+    end
+
+end
