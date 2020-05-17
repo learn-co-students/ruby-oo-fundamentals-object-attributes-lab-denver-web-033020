716f440b64365f27e6514102ee5da0f88bf94b01
LynnAmsbury <Lynn8542@gmail.com>
2020-05-17 17:36:59 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,19 @@
 class Dog
-    def name
-        @name
-    end
 
-    def breed
-        @breed
-    end
+  def name= new_name
+    @name = new_name
+  end
 
-    def name=(name)
-        @name = name
-    end
+  def name
+    @name
+  end
 
-    def breed=(breed)
-        @breed = breed
-    end
+  def breed= breed
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+
 end
